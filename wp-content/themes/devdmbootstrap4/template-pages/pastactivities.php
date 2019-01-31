5baac9968602a2a59f6a61034be6390a30493994
minminstyle <minminthein@gmail.com>
2019-01-31 23:38:22 +0800

    past-activities

--- wp-content/themes/devdmbootstrap4/template-pages/pastactivities.php
+++ wp-content/themes/devdmbootstrap4/template-pages/pastactivities.php
@@ -8,6 +8,18 @@ Template Name: Past Activities Page
 <?php get_template_part('template-parts/head'); ?>
 
 <?php get_template_part('template-parts/nav','header'); ?>
+<?php 
+
+
+$args = array(
+     );
+   
+    $posts = get_posts( $args );
+   
+ 
+
+
+?>
 
 <div class="container">
 
@@ -23,13 +35,42 @@ Template Name: Past Activities Page
   
  
     <div class="row" style="margin-top:20px;">  
-        <div class="col-sm-6">
-             
-         </div>
-        <div class="col-sm-6">
-             
+
+    <div class="col-sm-12">
+
+    <div id="accordion" class="accordion">
+        <div class="card mb-0">
+        <?php
+        $slideNo = 1; 
+        foreach( $posts as $post ) 
+        { 
+            
+            ?>
+            <h5 class="card-header">
+                <a data-toggle="collapse" href="#collapse_<?php echo $slideNo; ?>" aria-expanded="true" aria-controls="collapse-example" id="heading-example" class="d-block">
+                    <?php echo $post->post_title; ?>
+                    <i class="fa fa-chevron-down pull-right"></i>
+
+                </a>
+             </h5>
+            <div id="collapse_<?php echo $slideNo; ?>" class="collapse show" aria-labelledby="heading-example">
+                <div class="card-body">
+                        <?php echo $post->post_content; ?>
+                </div>
+            </div>
+ 
+        <?php 
+            
+            $slideNo +=1; 
+        } ?>
         </div>
-     </div>
+
+
+    </div>
+
+  
+    
+    </div>
 
        
 </div>
