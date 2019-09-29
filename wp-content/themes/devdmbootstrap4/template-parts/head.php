5d5cb1eee8afcac355f6c7eeb6e8c88520bde79a
AyeMin <ayemin@kootour.com>
2019-09-28 19:04:42 -0700

    menu-responsive

--- wp-content/themes/devdmbootstrap4/template-parts/head.php
+++ wp-content/themes/devdmbootstrap4/template-parts/head.php
@@ -2,8 +2,13 @@
 
     <div class="dmbs-header" style="background-color:#932a0c">
           <button class="navbar-toggler dmbs-header-nav-mobile-toggle smsc-mobile-button" type="button" data-toggle="collapse" data-target="#header-nav-content" aria-expanded="false" aria-label="Toggle navigation">
-                     <?php esc_html_e('Menu','devdmbootstrap4'); ?><span class="fa fa-bars"></span>
-                </button>
+                     <?php esc_html_e('','devdmbootstrap4'); ?><i class="fas fa-bars"></i>
+         </button>
+         <span class="social-media-icons-top">
+             <a href="https://www.meetup.com/en-AU/Mindfulness-Insight-Vipassana/" target="_blank"><i class="fab fa-meetup"></i></a>
+              <a href="https://www.facebook.com/people/Chanmyay-Yeikthabc/100006727365131" target="_blank"><i class="fab fa-facebook-square"></i></a>
+              <a href="https://www.youtube.com/user/ChanmyayYeikthaBC" target="_blank"><i class="fab fa-youtube"></i></a>
+         </span>
 
         <div class="text-center smsc-hidden"><img class="img-fluid img-logo-on-head" src="<?php echo get_template_directory_uri(); ?>/images/logo-300x300.png" alt="Logo">
         </div>
