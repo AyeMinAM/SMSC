9f6022cb9f33a254d7e697a476ad2e55ad69af16
AyeMin <ayemin@kootour.com>
2019-10-08 22:27:55 -0700

    smsc-about

--- wp-content/themes/devdmbootstrap4/template-pages/aboutsmsc.php
+++ wp-content/themes/devdmbootstrap4/template-pages/aboutsmsc.php
@@ -9,43 +9,50 @@ Template Name: About SMSC Page
 
 <?php get_template_part('template-parts/nav','header'); ?>
 
-<div class="container">
-
-    <div class="row" style="margin-top:20px;">
-        <div class="col-sm-13 img-container">
-  
-            <img src="<?php echo get_template_directory_uri(); ?>/images/about-group-photo.jpg" class="img-to-fit" >
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-13">
+            <img style="width: 100%" src="<?php echo get_template_directory_uri(); ?>/images/about-group-photo.jpg" class="img-to-fit" >
         </div>
     </div>
+</div>
 
-    <div class="row" style="margin-top:20px;" >
-         <div class="col-sm-12 text-center">
-                    <span class = "label label_prg_events_title">
-                        About SMSC</span>
-          </div>
+<div class="container">
+    <div class="card-smsc about-smsc-bg">
+        <h4 class="label smsc-title style="margin-bottom:1rem">About SMSC</h4>
     
-    </div>
-    
-    <div class="row" style="margin-top:20px;" >
-
-        <div class="col-sm-13">
         
         <p class="text-justify">The Satipatthana Meditation Society of Canada (SMSC) is a Buddhist organization based in Vancouver, British Columbia. The Society is incorporated under the Society Act in British Columbia and holds registered not-for-profit charity status through the Canada Revenue Agency.</p>
-    
-            
-        <p class="text-justify">
 
-        The purpose of the Society is to practice and discuss Theravada Buddhism with serious practitioners and to promote and provide education to anyone interested in exploring Theravada Buddhism. The Society&apos;s approach is built around Satipatthana Vipassana (Pure Vipassana) meditation, which is commonly known in the West as Insight Through Mindfulness meditation. The Society offers classes, sitting meditation, monthly residential retreats as the various formats of communication available to interested parties. 
-        </div>
+        <p class="text-justify">The purpose of the Society is to practice and discuss Theravada Buddhism with serious practitioners and to promote and provide education to anyone interested in exploring Theravada Buddhism. The Society&apos;s approach is built around Satipatthana Vipassana (Pure Vipassana) meditation, which is commonly known in the West as Insight Through Mindfulness meditation. The Society offers classes, sitting meditation, monthly residential retreats as the various formats of communication available to interested parties. </p>
+
+        <img src="<?php echo get_template_directory_uri(); ?>/images/divider-flower.png" class="divider-flower">
+
+        <h4 class="label smsc-title" style="margin-bottom:1rem">Our Practise</h4>
+
+        <p class="text-justify">The satipatthana vipassana meditation that the Society adheres to is directly based on the Four Foundations of Mindfulness taught by the Buddha in the Satipatthana Discourse. The late Venerable Mahasi Sayadaw of Myanmar popularized this tradition in 1960&apos;s and 70&apos;s to the world outside of Myanmar. The practice is the pure Vipassana method, rather than the mixed (Samatha-Vipassana) method. Even those with no previous meditation experience can experience results in a relatively short period of time if they are earnest and hard working. Mahasi Sayadaw&apos;s chief disciples, the Venerable Sayadaws U Khundala, U Pandita, the late U Silananda and U Janaka continued to spread this technique throughout the world from the 1980&apos;s to the present time. </p>
+
+        <p class="text-justify">
+        Generosity, morality, mindfulness, concentration and intuitive understanding are the main pillars of the practice. The SMSC welcomes people of all faiths and ethnic backgrounds. A genuine belief in the necessity for loving kindness and compassion in the world is the only prerequisite required. 
+        </p>
+
+        <img src="<?php echo get_template_directory_uri(); ?>/images/divider-flower.png" class="divider-flower">
+
+        <h4 class="label smsc-title" style="margin-bottom:1rem">Our Vision</h4>
+        <p class="text-justify">The satipatthana vipassana meditation that the Society adheres to is directly based on the Four Foundations of Mindfulness taught by the Buddha in the Satipatthana Discourse. The late Venerable Mahasi Sayadaw of Myanmar popularized this tradition in 1960&apos;s and 70&apos;s to the world outside of Myanmar. The practice is the pure Vipassana method, rather than the mixed (Samatha-Vipassana) method. Even those with no previous meditation experience can experience results in a relatively short period of time if they are earnest and hard working. Mahasi Sayadaw&apos;s chief disciples, the Venerable Sayadaws U Khundala, U Pandita, the late U Silananda and U Janaka continued to spread this technique throughout the world from the 1980&apos;s to the present time. </p>
+        <p class="text-justify">
+         Generosity, morality, mindfulness, concentration and intuitive understanding are the main pillars of the practice. The SMSC welcomes people of all faiths and ethnic backgrounds. A genuine belief in the necessity for loving kindness and compassion in the world is the only prerequisite required. 
+        </p>
     </div>
+</div>
     
-    
+
+<!-- <div class="container">  
     <div class="row" style="margin-top:20px;">
         <div class="col-sm-12 text-center">
         <img src="<?php echo get_template_directory_uri(); ?>/images/divider.png" class="smsc_img_divider" alt="divider">           
         </div>
     </div>
-
     <div class="row" style="margin-top:20px;">
             <div class="row">
                     <div class="col-sm-12">
@@ -55,21 +62,13 @@ Template Name: About SMSC Page
             </div>
             <div class="row" style="margin-top:20px;">
                     <div class="col-sm-9">
-                    <p class="text-justify">The satipatthana vipassana meditation that the Society adheres to is directly based on the Four Foundations of Mindfulness taught by the Buddha in the Satipatthana Discourse. The late Venerable Mahasi Sayadaw of Myanmar popularized this tradition in 1960&apos;s and 70&apos;s to the world outside of Myanmar. The practice is the pure Vipassana method, rather than the mixed (Samatha-Vipassana) method. Even those with no previous meditation experience can experience results in a relatively short period of time if they are earnest and hard working. Mahasi Sayadaw&apos;s chief disciples, the Venerable Sayadaws U Khundala, U Pandita, the late U Silananda and U Janaka continued to spread this technique throughout the world from the 1980&apos;s to the present time. 
-                    </p>
-                    <p class="text-justify">
-                    Generosity, morality, mindfulness, concentration and intuitive understanding are the main pillars of the practice. The SMSC welcomes people of all faiths and ethnic backgrounds. A genuine belief in the necessity for loving kindness and compassion in the world is the only prerequisite required. 
-	
-                    </p>
+                    
                     </div>
                     <div class="col-sm-3 text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/retreat-annual.jpg"  class="img-rounded smsc_img_beginner"  alt="beginner">
                     </div>
-
-                    
             </div>      
     </div>
-
     <div class="row" style="margin-top:20px;">
             <div class="row">
                     <div class="col-sm-12">
@@ -98,8 +97,7 @@ Template Name: About SMSC Page
         <img src="<?php echo get_template_directory_uri(); ?>/images/divider.png" class="smsc_img_divider" alt="divider">           
         </div>
     </div>
-
-</div>
+</div> -->
 
 <?php get_template_part('template-parts/nav','footer'); ?>
 
